adc99f1c988e0bce1ff7c652c73d8abd4ed940e6
Yoav Orot <orot.yoav@gmail.com>
2021-05-21 12:24:31 +0300

    Set customization file path and add it to gitignore

--- init.el
+++ init.el
@@ -391,4 +391,11 @@ DIR must include a .project file to be considered a project."
 ;; Show time in the modeline
 (display-time-mode 1)
 
+;; Set customization file path
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+;; Load customization file
+(when (file-exists-p custom-file)
+  (load custom-file))
+
 ;;; init.el ends here
