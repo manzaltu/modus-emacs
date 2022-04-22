3e3cc4aeba57bc42749ac8379aa92b743d53d137
Yoav Orot <orot.yoav@gmail.com>
2022-04-21 20:23:32 -0500

    quick menu: Add util subgroup

--- init.el
+++ init.el
@@ -128,6 +128,7 @@
  "s" '(:which-key "Search")
  "v" '(:which-key "View")
  "w" '(:which-key "Window")
+ "x" '(:which-key "Utils")
  "t" '(:which-key "Tab")
  "h" '(:which-key "Help")
  "p" '(:which-key "Project")
