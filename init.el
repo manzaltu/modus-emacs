9ba829d26e32b65a48ab025e19cbf2d9b32ff58d
Yoav Orot <orot.yoav@gmail.com>
2022-11-01 09:02:09 -0400

    lsp-ui: Change ui doc position to bottom

--- init.el
+++ init.el
@@ -1032,7 +1032,7 @@ run the attached function (if exists) and enable lsp"
   ;; Do not show documentation automatically
   (setq lsp-ui-doc-show-with-mouse nil)
   (setq lsp-ui-doc-max-height 60)
-  (setq lsp-ui-doc-position 'at-point))
+  (setq lsp-ui-doc-position 'bottom))
 
 ;; Init lsp-treemacs for an interactive lsp tree-like interface
 (use-package lsp-treemacs
