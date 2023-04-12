6ee726f298083285e1afc080dc4154d5456f122b
Yoav Orot <orot.yoav@gmail.com>
2023-04-12 12:35:12 -0400

    consult: Change find file project switch command to use fd

--- init.el
+++ init.el
@@ -869,7 +869,7 @@ directory as a fall back."
   ;; On project switch, use consult for file and regexp search
   (setq project-switch-commands
         '( (consult-project-buffer "Buffer" ?b)
-           (consult-find "Find file" ?f)
+           (consult-fd "Find file" ?f)
            (consult-ripgrep "Ripgrep" ?r)
 	   (magit-status "Magit" ?g))))
 
