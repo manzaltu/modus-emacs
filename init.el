f3f72dbb12e9bb3bafe0e1c5f130f75d111aac39
Yoav Orot <orot.yoav@gmail.com>
2023-05-10 19:03:30 -0400

    format-all-region: Remove all-buffer command key binding

--- init.el
+++ init.el
@@ -1946,8 +1946,7 @@ run the attached function (if exists) and enable lsp"
   :general
   ( :keymaps 'mo-quick-menu-map
     :prefix "c"
-    "f" #'format-all-region
-    "F" #'format-all-buffer)
+    "f" #'format-all-region)
   ( :keymaps 'mo-quick-menu-map
     :prefix "b"
     "f" #'format-all-mode)
