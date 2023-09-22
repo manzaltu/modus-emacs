a8f3295e4cb6f5b076f94084e47d057ce065489c
Yoav Orot <orot.yoav@gmail.com>
2023-09-22 14:00:44 -0400

    org-journal: Disable task carryover

--- init.el
+++ init.el
@@ -907,7 +907,8 @@ Ask for action even on single candidate jumps."
     (concat (file-name-as-directory org-directory) "journal"))
   ( org-journal-file-type 'weekly)
   ( org-journal-enable-agenda-integration t)
-  ( org-journal-file-format "%Y%m%d.org"))
+  ( org-journal-file-format "%Y%m%d.org")
+  ( org-journal-carryover-items ""))
 
 ;; Init org-super-agenda for grouping agenda items into sections
 (use-package org-super-agenda
