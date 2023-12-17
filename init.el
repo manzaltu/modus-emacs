ea31a9db7772ec27cd5ddba79585221814d61aff
Yoav Orot <orot.yoav@gmail.com>
2023-12-17 12:19:20 -0500

    consult: Change register load, store & view command quick menu bindings

--- init.el
+++ init.el
@@ -1549,9 +1549,9 @@ directory as a fall back."
   ( "C-x 4 b" #'consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
   ( "C-x 5 b" #'consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
   ;; Custom M-# bindings for fast register access
-  ( "M-\"" #'consult-register-load)
-  ( "M-'" #'consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
-  ( "C-M-\"" #'consult-register)
+  ( "C-~" #'consult-register-load)
+  ( "M-~" #'consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+  ( "C-M-~" #'consult-register)
   ;; Other custom bindings
   ( "M-y" #'consult-yank-pop)                ;; orig. yank-pop
   ;; M-g bindings (goto-map)
