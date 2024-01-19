5ca0c8e4f9139dbf7553a744d3e36a4aa73f6f25
Yoav Orot <orot.yoav@gmail.com>
2024-01-19 18:23:12 -0500

    modus-operandi-emacs: Change close project with tab command key binding

--- init.el
+++ init.el
@@ -525,7 +525,7 @@ the user to input the run command."
   ( :keymaps 'mo-quick-menu-map
     :prefix "RET"
     "SPC" #'mo-open-project-with-tab
-    "ESC" #'mo-close-project-with-tab)
+    "DEL" #'mo-close-project-with-tab)
   :preface
   (defun mo-copy-file-path ()
     "Copy the full path of the current buffer's file."
