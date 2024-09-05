28de10877d71849284f139c9a19d5b2786114a6e
Yoav Orot <orot.yoav@gmail.com>
2024-09-05 13:04:46 -0400

    elisp-mode: Correct eval-last-sexp in normal state

--- init.el
+++ init.el
@@ -2532,7 +2532,9 @@ When a prefix ARG is given always prompt for a command to use."
   :general
   ( :keymaps '( emacs-lisp-mode-map lisp-interaction-mode-map)
     "C-M-s-e" #'eval-defun
-    "C-M-s-s" #'eval-last-sexp))
+    "C-M-s-s" #'eval-last-sexp)
+  :config
+  (advice-add 'eval-last-sexp :around #'mo-evil-correct-last-sexp))
 
 ;; Init sly for Common Lisp support
 (use-package sly
