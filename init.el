d2143129cd9c4dcb34f14712bbc2f79609686d9b
Yoav Orot <orot.yoav@gmail.com>
2024-09-23 09:14:20 -0400

    org: Add mark ring goto command key binding

--- init.el
+++ init.el
@@ -1099,7 +1099,8 @@ Used for preventing recursion when recording new jumps.")
     "C-M-s-i" #'org-insert-structure-template
     "C-M-s-e" #'org-edit-special
     "C-M-s-S-e" #'org-export-dispatch
-    "C-M-s-<return>" #'org-open-at-point)
+    "C-M-s-<return>" #'org-open-at-point
+    "C-M-s-," #'org-mark-ring-goto)
   ( :keymaps 'org-src-mode-map
     "C-M-s-e" #'org-edit-src-exit
     "C-M-s-k" #'org-edit-src-abort)
