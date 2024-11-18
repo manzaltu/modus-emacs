4e6c6b5ba3543585a0e6542c857b72421bddca50
Yoav Orot <orot.yoav@gmail.com>
2024-11-18 11:27:47 -0500

    Add org-ql for advanced org search functionality

--- init.el
+++ init.el
@@ -1387,6 +1387,9 @@ Used for preventing recursion when recording new jumps.")
   :config
   (consult-org-roam-mode))
 
+;; Init org-ql for advanced org search functionality
+(use-package org-ql)
+
 ;; Init org-modern for a modern org buffer style
 (use-package org-modern
   :functions global-org-modern-mode
