67b95a642a26f2064c5d2a6e3b921dd72e55eb33
Yoav Orot <orot.yoav@gmail.com>
2025-02-14 12:01:05 -0500

    gptel: Move quick menu command key bindings to the AI group

--- init.el
+++ init.el
@@ -3690,6 +3690,7 @@ run the attached function (if exists) and enable lsp"
 (use-package gptel
   :general
   ( :keymaps 'mo-quick-menu-map
+    :prefix "TAB"
     "TAB" #'gptel
     "<backtab>" #'gptel-send)
   ( :keymaps 'mo-quick-menu-map
