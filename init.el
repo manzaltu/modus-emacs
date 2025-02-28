89e4edb904ff4b766b913ec5d9a285b93f4ec111
Yoav Orot <orot.yoav@gmail.com>
2025-02-27 19:20:54 -0500

    lsp: Remove lsp describe command key binding from quick menu
    Using embark instead.

--- init.el
+++ init.el
@@ -2839,10 +2839,6 @@ When a prefix ARG is given always prompt for a command to use."
   ( :keymaps 'mo-quick-menu-map
     :prefix "n"
     "l" #'lsp-org)
-  (mo-quick-menu-definer
-    :definer 'minor-mode
-    :keymaps 'lsp-mode
-    "h h" #'lsp-describe-thing-at-point)
   ( :keymaps 'rustic-mode-map
     "C-M-s-c" #'lsp-rust-analyzer-open-cargo-toml)
   (mo-quick-menu-definer
