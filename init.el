a3a2619f754cc716fce013d8b34e89927eca3e88
Yoav Orot <orot.yoav@gmail.com>
2025-03-15 14:59:44 -0400

    lsp-mode: Remove open rust cargo toml command key binding

--- init.el
+++ init.el
@@ -2832,8 +2832,6 @@ Used while preview is toggled off."
     :definer 'minor-mode
     :keymaps 'lsp-mode
     "h h" #'lsp-describe-thing-at-point)
-  ( :keymaps 'rustic-mode-map
-    "C-M-s-c" #'lsp-rust-analyzer-open-cargo-toml)
   (mo-quick-menu-definer
     :keymaps 'rustic-mode-map
     "h H" #'lsp-rust-analyzer-open-external-docs)
