03d681b2a7ffe80c5979a749f4009708bbd6e45b
Yoav Orot <orot.yoav@gmail.com>
2025-04-22 12:06:16 -0400

    modus-operandi-emacs: Decrease font size

--- init.el
+++ init.el
@@ -66,7 +66,7 @@
 ;; Optionally, load personal settings
 (load (concat (file-name-directory load-file-name) "personal.el") t)
 
-(defvar mo-font "PragmataPro Mono Liga-14"
+(defvar mo-font "PragmataPro Mono Liga-12"
   "Font to use.")
 
 ;; Add general.el key mapper
