3ec97e3de347d0add8ac99ed40aa2edfaf9111bf
Yoav Orot <orot.yoav@gmail.com>
2025-08-21 16:22:28 -0400

    saveplace-pdf-view: Remove redundant save-place-mode initialization

--- init.el
+++ init.el
@@ -1607,9 +1607,7 @@ Used for preventing recursion when recording new jumps.")
 
 ;; Init saveplace-pdf-view for saving the last location in a pdf document
 (use-package saveplace-pdf-view
-  :after bookmark
-  :config
-  (save-place-mode 1))
+  :after bookmark)
 
 ;; Init nov for reading epub files in Emacs
 (use-package nov
