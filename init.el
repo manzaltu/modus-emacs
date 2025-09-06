0e1cc4e78f513d5195049b47cf3655ef4e1d897d
Yoav Orot <orot.yoav@gmail.com>
2025-09-06 17:24:46 -0500

    consult: Change line symbol at point command quick menu key binding

--- init.el
+++ init.el
@@ -2130,7 +2130,7 @@ Returns the selected project root directory or nil if cancelled."
     :prefix "j"
     "b" #'consult-project-buffer)
   ( :keymaps 'mo-quick-menu-map
-    "*" #'mo-consult-line-symbol-at-point
+    "]" #'mo-consult-line-symbol-at-point
     "&" #'mo-consult-line-symbol-at-point-other-window)
   ( :keymaps 'mo-quick-menu-map
     :prefix "v"
