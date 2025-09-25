c409003bffd77f955e2b27784bd11987af319fb9
Yoav Orot <orot.yoav@gmail.com>
2025-09-25 11:50:10 -0400

    consult: Change minor mode menu command quick menu key binding

--- init.el
+++ init.el
@@ -2081,7 +2081,7 @@ Returns the selected project root directory or nil if cancelled."
     "C-<menu>" #'consult-imenu-multi
     "," #'consult-focus-lines
     "." #'consult-keep-lines
-    "m" #'consult-minor-mode-menu
+    "M" #'consult-minor-mode-menu
     "B" #'consult-recent-file)
   ( :keymaps 'mo-quick-menu-map
     :prefix "DEL"
