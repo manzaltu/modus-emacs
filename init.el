68d4457b7ca7f320e2dfe17b38ed17c9e8c0fe92
Yoav Orot <orot.yoav@gmail.com>
2026-06-21 11:50:22 +0300

    diff-hl: Add merge-base reference command key binding to the quick menu

--- init.el
+++ init.el
@@ -3272,6 +3272,7 @@ the produced output, not the process exit code)."
   :general
   ( :keymaps 'mo-quick-menu-map
     :prefix "g"
+    "r" #'mo-diff-hl-set-reference-merge-base-in-project
     "M-r" #'diff-hl-set-reference-rev-in-project
     "]" #'diff-hl-next-hunk
     "[" #'diff-hl-previous-hunk)
