eea8dfd798887d0fae7c1f7c630e064a6364d2dd
Yoav Orot <orot.yoav@gmail.com>
2026-08-08 18:40:39 -0400

    difftastic: Enable magit key bindings

--- init.el
+++ init.el
@@ -3219,7 +3219,11 @@ leaving the heading outside of the visible region."
   :after magit)
 
 ;; Init difftastic for structural diffs
-(use-package difftastic)
+(use-package difftastic
+  :commands difftastic-bindings-mode
+  :init
+  ;; Bind difftastic commands in the relevant magit prefixes
+  (difftastic-bindings-mode))
 
 ;; Init git-modes for editing git config files
 (use-package git-modes)
