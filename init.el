c3356f58572f10a835a961dccf5da818a8d64d19
Yoav Orot <orot.yoav@gmail.com>
2026-08-08 13:50:10 -0400

    treemacs-icons-dired: Add missing function declaration

--- init.el
+++ init.el
@@ -3012,6 +3012,7 @@ leaving the heading outside of the visible region."
 
 ;; Init treemacs-icons for icons in dired
 (use-package treemacs-icons-dired
+  :functions treemacs-icons-dired-mode
   :config
   (treemacs-icons-dired-mode))
 
