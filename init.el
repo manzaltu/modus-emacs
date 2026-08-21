8fde4b3944db1cf48fd9198797868e63ad2cffa3
Yoav Orot <orot.yoav@gmail.com>
2026-08-21 15:28:16 -0400

    jinx: Add correct word command key binding

--- init.el
+++ init.el
@@ -5968,6 +5968,7 @@ If project root cannot be found, use the buffer's default directory."
     "s" #'jinx-correct-all
     "C-s" #'jinx-mode)
   ( :keymaps 'override
+    "M-$" #'jinx-correct-word
     "M-+" #'jinx-correct-nearest)
   :hook ( emacs-startup . global-jinx-mode))
 
