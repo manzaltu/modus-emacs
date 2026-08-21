bf24417b831f686e605f70f74f81bcb0e8c735ec
Yoav Orot <orot.yoav@gmail.com>
2026-08-21 14:45:59 -0400

    devdocs: Move lookup command key binding to embark identifier map

--- init.el
+++ init.el
@@ -5189,9 +5189,12 @@ enabling lsp."
 ;; Init devdocs for viewing online dev documentation
 (use-package devdocs
   :general
-  ( :keymaps 'mo-quick-menu-map
-    :prefix "c"
-    "h" #'devdocs-lookup)
+  ( :keymaps 'embark-identifier-map
+    "k" #'devdocs-lookup)
+  :init
+  ;; When run as an embark action, keep the target as editable initial input
+  (cl-pushnew #'embark--allow-edit
+              (alist-get 'devdocs-lookup embark-target-injection-hooks))
   :config
   (setq devdocs-data-dir (mo-cache-path "devdocs")))
 
