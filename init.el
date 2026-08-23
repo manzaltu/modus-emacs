4b4e6c03f0d9759e2d4b1a7713e4948dd6aaa88e
Yoav Orot <orot.yoav@gmail.com>
2026-08-23 16:49:17 -0400

    ffap: Remove command key binding from the quick menu
    Embark targets the file at point and offers the same actions on it.

--- init.el
+++ init.el
@@ -5219,10 +5219,6 @@ enabling lsp."
 ;; Init ffap for find-file-at-point
 (use-package ffap
   :straight nil
-  :general
-  ( :keymaps 'mo-quick-menu-map
-    :prefix "f"
-    "p" #'ffap)
   :config
   (setq ffap-machine-p-known 'reject))
 
